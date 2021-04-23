cc25c3ec55b763bcd12d6165fb7491685d4c1fe7
zyj <893722059@qq.com>
2021-04-23 17:25:45 +0800

    a

--- pages/binding/index.js
+++ pages/binding/index.js
@@ -4,35 +4,91 @@ const app = getApp()
 
 Page({
   data: {
-    array: ['管理用户', '驾驶人员'],
-    objectArray: [
-      {
-        id: 0,
-        name: '管理用户'
-      },
-      {
-        id: 1,
-        name: '驾驶人员'
-      }
-    ],
-    index: 0,
     phone:'',
-    name:'',
-    type:''
+    code:'',
+    sendTime: '获取验证码',
+    sendColor: '#363636',
+    snsMsgWait: 60,
   },
+  /**
+   * 获取手机验证码
+   */
+  getverifycode: function (e) {
+    var that = this;
+
+    if (this.data.phone == "" || this.data.phone == undefined) {
+      wx.showToast({
+        title: '手机号不能为空',
+        icon: 'none',
+        duration: 1500
+      })
+      return;
+    }
+    if (!(/^1[3|4|5|8][0-9]\d{8}$/.test(this.data.phone))) {
+      wx.showToast({
+        title: '手机号输入有误',
+        icon: 'none',
+        duration: 1500
+      })
+      return;
+    }
+    // 60秒后重新获取验证码
+    var inter = setInterval(function () {
+      this.setData({
+        smsFlag: true,
+        sendColor: '#cccccc',
+        sendTime: this.data.snsMsgWait + 's后重发',
+        snsMsgWait: this.data.snsMsgWait - 1
+      });
+      if (this.data.snsMsgWait < 0) {
+        clearInterval(inter)
+        this.setData({
+          sendColor: '#363636',
+          sendTime: '发送验证码',
+          snsMsgWait: 60,
+          smsFlag: false
+        });
+      }
+    }.bind(this), 1000);
+
+    wx.request({
+      url: app.globalData.apiUlr + 'user/verifycode',
+      method: 'get',
+      data: {
+        phone: this.data.inputPhone
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'accept': 'application/json'
+      },
+      success(res) {
+        if (res.data.code != 1) {
+          that.toast(res.data.msg);
+        }
+        else {
+          console.log('短信验证码发送成功');
+
+        }
+
+      }
+    })
+  },
+
+  /**
+   * 获取手机号
+   */
   bindKeyInputPhone:function(e){
     this.setData({
       phone: e.detail.value
     })
   },
+
+  /**
+   * 获取手机验证码
+   */
   bindKeyInputName:function(e){
     this.setData({
-      name: e.detail.value
-    })
-  },
-  bindPickerChange: function (e) {
-    this.setData({
-      index: e.detail.value
+      code: e.detail.value
     })
   },
   clickBinding:function(){
